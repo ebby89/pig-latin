b7bf7d1c01b72e041f581244950f7f2a1f165f7e
Kyle Ebersole and Rane Fields <kyle@>
2017-09-20 10:26:48 -0700

    Add filter to move consonants to the end of the word

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
-var firstVowelRegex = /^[a,e,i,o,u]/i
+var firstVowelRegex = /[a,e,i,o,u]/i
 
 
 var toPigLatin = function(word) {
-  if (firstVowelRegex.test(word)) {
+  var firstVowelIndex = word.search(firstVowelRegex);
+
+  if (firstVowelIndex === 0) {
     return word + 'way';
-  };
+  }
+  else {
+    var firstConsonants = word.slice(0, firstVowelIndex);
+    var moveConsonants = word.substring(firstVowelIndex) + firstConsonants;
+    return moveConsonants + "ay";
+  }
 
 
 
 
   return word;
-}
+};
 
 
 
